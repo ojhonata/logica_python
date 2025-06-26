fbf8932b9b7474aecb9e155d8bbd693f577e6988
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-06-26 17:21:13 -0300

    Adicionando a função update

--- app_db.py
+++ app_db.py
@@ -11,11 +11,29 @@ try:
 
     print('conexão bem sucedida')
 
-    cursor = conexao_db.cursor()
-
-    cursor.execute('select * from produto')
-
-    resultado = cursor.fetchall()
-    print(resultado)
 except:
     print('!!!!')
+
+def read_db():
+    cursor = conexao_db.cursor() # usado para escrever comandos sql
+    cursor.execute('select * from produto')
+    resultado = cursor.fetchall() # busca todos os resultados de uma consulta sql feita no cursor em forma de tuplas
+    for i in resultado:
+        print(i)
+
+read_db()
+
+def update_product(id, quantidade):
+    try:
+        cursor = conexao_db.cursor()
+        sql = 'UPDATE produto SET quantidade = %s WHERE produto_id = %s'
+
+        valor = (quantidade, id)
+        cursor.execute(sql, valor)
+
+        conexao_db.commit()
+        print('Alteração realizada')
+    except Exception as e:
+        print('!!!!!!!', e)
+
+update_product(2, 10)
