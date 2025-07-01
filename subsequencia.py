7ef79da4cca5edc27db0d9aba37a7114ff901a90
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-07-01 16:35:13 -0300

    Adicionando comentários explicativos

--- subsequencia.py
+++ subsequencia.py
@@ -1,23 +1,18 @@
-'''pegamos a sequencia de numeros e adicionamos em um dicionario junto a um contador as letras com que se repetem
-
- '''
-
 def subsequencia(sequencia):
     num_repitidas = {}
     maior = 0
-    for i in sequencia:
-        if i in num_repitidas:
-            num_repitidas[i] += 1
+    for i in sequencia: # passa número por número do parametro
+        if i in num_repitidas: # se o numero já estiver no dicionário
+            num_repitidas[i] += 1 # soma mais um no contador
         else:
-            num_repitidas[i] = 1
-    for chave, valor in num_repitidas.items():
-        if chave + 1 in num_repitidas:
-            soma = valor + num_repitidas[chave + 1]
-            if soma > maior:
-                maior = soma
-                
+            num_repitidas[i] = 1 # senão o contador será igual a 1
+    for chave, valor in num_repitidas.items(): # percorre chave e valor no dicionario
+        if chave + 1 in num_repitidas: # se a chave atual + 1 (1 + 2) estiver no dicionario
+            soma = valor + num_repitidas[chave + 1] # soma os valores das duas chaves
+            if soma > maior: # se a soma for maior que o valor atual de 'maior'
+                maior = soma # atualiza 'maior' com essa nova soma
     return maior, num_repitidas
 
-termo = [1,1,1,1, 2, 3, 3, 3, 4, 4, 5]
+termo = [1, 1, 1, 1, 2, 3, 3, 3, 4, 4, 5,]
 funcao = subsequencia(termo)
 print(funcao)
