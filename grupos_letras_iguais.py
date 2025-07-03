96b659c409423f66ca1fcbd65b58e7841a785c20
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-07-03 15:13:13 -0300

    Ajustes

--- grupos_letras_iguais.py
+++ grupos_letras_iguais.py
@@ -1,9 +1,9 @@
 def grupos_letras_iguais(letras):
     contador = 0
-    ultima_letra = '' # varialve onde será aramzenada a ultima letra
+    ultima_letra = '' # varável onde será aramzenada a última letra
     for letra in letras: # para cada letra em letras
         
-        if ultima_letra != letra: # se a última letra for diferente ds letra atual
+        if ultima_letra != letra: # se a última letra for diferente da letra atual
             ultima_letra = letra # adiciona letra na variável ultima_letra
             contador += 1
     return contador
--- proximo_10.py
+++ proximo_10.py
@@ -4,7 +4,7 @@ def proximo_a_dez(numeros, tolerancia=2):
         if num + 1 in numeros: # se o número atual + 1 estiver no parametro 'numeros'
             resultado = num + (num + 1) # soma o número atual + o próximo número
             diferenca = abs(resultado - 10) # subtraimos o resultado por 10, se o número for negativo ele torna a ser absoluto/positivo (abs)
-        print(f'{resultado} - {diferenca}', True if diferenca <= tolerancia else False) # imprimos o resultado da soma - diferença e verificamos se o número é próximo a 10 com a tolerancia que foi difinada no parâmetro da função, se a diferença for menor ou igual a 2 exibimos True.
+        print(f'{resultado} - {diferenca}', True if diferenca <= tolerancia else False) # imprime o resultado da soma - diferença e verificamos se o número é próximo a 10 com a tolerancia que foi difinada no parâmetro da função, se a diferença for menor ou igual a 2 exibimos True.
 
 numeros = [1, 2, 3, 4, 5]
 print(numeros)
