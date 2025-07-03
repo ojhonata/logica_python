a996967eaa7290fc100b2aaf855a1034fbb39cc2
Jhonata Gabriel Canevare <152428631+ojhonata@users.noreply.github.com>
2025-07-03 19:33:11 -0300

    Adicionando comentários explicativos

--- api_gemini.py
+++ api_gemini.py
@@ -1,16 +1,22 @@
-import google.generativeai as genai
+import google.generativeai as genai # google.generativeai importa a parte específica do gemini IA
 
-genai.configure(api_key="AIzaSyCyLGUZ3j_Z5kargy23bI7KENXu9m26DoI")
-model= genai.GenerativeModel(model_name="gemini-2.0-flash")
+genai.configure(api_key="AIzaSyCyLGUZ3j_Z5kargy23bI7KENXu9m26DoI") # defina a chave da api
+model= genai.GenerativeModel(model_name="gemini-2.0-flash") # defini o modelo da IA
 
 historico = []
 while True:
     usuario = input('Você: ')
 
     if usuario:
-        historico.append({'role': 'user', 'parts': [usuario]})
-        response = model.generate_content(historico)
+        historico.append({'role': 'user', 'parts': [usuario]}) # adiciona o texto do usuário na lista vazia
+        response = model.generate_content(historico) # defini o modelo da IA, função que envia o prompt da lista de historico
+        historico.append({'role': 'model', 'parts': [response.text]}) # adiciona a resposta da IA no histórico
         print(f'Gemini: {response.text}')
-        historico.append({'role': 'model', 'parts': [response.text]})
     else:
         break
+
+'''
+{'role': 'user', 'parts': [usuario]} 
+role = indique quem está falando (user, model)
+parts = retorna uma lista com a resposta contendo texto, imagens, etc..
+'''
